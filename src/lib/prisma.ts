bccd8b9b844bb5345f05545d5b70cfc4e3c69a4a
ghalitsar <ghalitsardev@gmail.com>
2025-05-19 17:14:21 +0700

    update prisma client error

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,4 @@
 import { PrismaClient } from "@prisma/client";
-import { neon } from "@neondatabase/serverless";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
@@ -22,12 +21,6 @@ const prismaClientSingleton = () => {
             },
           }
         : undefined,
-        
-    // Konfigurasi reconnect otomatis ketika koneksi terputus
-    connection: {
-      reconnect: true,
-      maxConnections: 5,
-    },
   });
 };
 
